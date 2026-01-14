c0a5a87030a4b45251dc4db6f057ccf8e8b2871e
Joshualu <j.moses@alustudent.com>
2026-01-14 16:25:21 +0200

    task 7 retry

--- csharp-hello_world/7-concat/obj/Debug/net7.0/7-concat.AssemblyInfo.cs
+++ csharp-hello_world/7-concat/obj/Debug/net7.0/7-concat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("7-concat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9948115569b84e770395140e53d7f158a3408a63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8270bc437d66c5ef7651442de6cd0a09562a2d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("7-concat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("7-concat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
