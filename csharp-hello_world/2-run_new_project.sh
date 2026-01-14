8c887f96984b91bb458037eeafafed6a8d84bda3
Joshualu <j.moses@alustudent.com>
2026-01-14 11:12:24 +0200

    task 2

---
+++ csharp-hello_world/2-run_new_project.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Initialize a new C# console project in a folder named 2-new_project
+dotnet new console -o 2-new_project
+
+# Modify the .csproj file to target .NET 7.0
+sed -i 's/<TargetFramework>.*<\/TargetFramework>/<TargetFramework>net7.0<\/TargetFramework>/g' 2-new_project/2-new_project.csproj
+
+# Build the project
+dotnet build 2-new_project
+
+# Run the project
+dotnet run --project 2-new_project
