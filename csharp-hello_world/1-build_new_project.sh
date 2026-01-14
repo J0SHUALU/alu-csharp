7642770ae0c29f92f9c8a5b8928b808a7623c06b
Joshualu <j.moses@alustudent.com>
2026-01-14 11:06:01 +0200

    task 1

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -3,5 +3,8 @@
 # Initialize a new C# console project in a folder named 1-new_project
 dotnet new console -o 1-new_project
 
+# Modify the .csproj file to target .NET 7.0
+sed -i 's/<TargetFramework>.*<\/TargetFramework>/<TargetFramework>net7.0<\/TargetFramework>/g' 1-new_project/1-new_project.csproj
+
 # Build the project
 dotnet build 1-new_project
--- csharp-hello_world/1-new_project/obj/Debug/net10.0/1-new_project.AssemblyInfo.cs
+++ csharp-hello_world/1-new_project/obj/Debug/net10.0/1-new_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1-new_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32217964640cb2c041f68a499509028571703e65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd5a3d7ba587bdf7b9bb8625bae8bc833a5b7ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("1-new_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1-new_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
