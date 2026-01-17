a25fdd1976d7985983e8d0d2f63a12b76f659acb
Joshualu <j.moses@alustudent.com>
2026-01-17 12:29:57 +0200

    task 7

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -13,17 +13,8 @@ class Character
 
 class Program
 {
-	static void Main(string[] args)
+	static void Main()
 	{
-		char[] letters = { 'a', 'A', 'Q', 'h', '9', 'B', 'g'};
-		int i;
-
-		for (i = 0; i < letters.Length; i++)
-		{
-			if(Character.IsLower(letters[i]))
-				Console.WriteLine("{0} is lowercase", letters[i]);
-			else
-				Console.WriteLine("{0} is uppercase", letters[i]);
-		}
+		Console.WriteLine("");
 	}
 }
