d7883c903fc3c59b03a2d6a609520762ce980f58
Joshualu <j.moses@alustudent.com>
2026-01-21 10:44:48 +0200

    task 0

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,53 @@
+using System;
+
+class Array
+{
+    public static int[]? CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+            Console.Write(newArray[i]);
+            if (i < size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+        return newArray;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[]? newArray;
+
+        newArray = Array.CreatePrint(10);
+        if (newArray != null)
+            Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        
+        newArray = Array.CreatePrint(16);
+        if (newArray != null)
+            Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        
+        newArray = Array.CreatePrint(0);
+        if (newArray != null)
+            Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        
+        newArray = Array.CreatePrint(-10);
+    }
+}
